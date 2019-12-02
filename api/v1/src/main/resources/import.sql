9d51352a9be27e21ba0ceb11b2e13282155d6851
kersevan <gregor.kersevan@gmail.com>
2019-12-02 19:05:36 +0100

    small import.sql changes

--- api/v1/src/main/resources/import.sql
+++ api/v1/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO analytics(id, timstamp, product_id,  numberOfOrders, income) VALUES('92d0e296-96fa-432e-ba22-db529630ef2a', NOW(), '9caaaa3c-3843-44e0-ba6e-505032c4eeeb', 9, 1099.99);
 INSERT INTO analytics(id, timstamp, product_id,  numberOfOrders, income) VALUES('463c9c18-7a56-42f2-af83-0f4290ece62b', NOW(), '56e16edf-0605-45bb-8ea3-b34b0d4c9fb7', 8, 700.45);
-INSERT INTO analytics(id, timstamp, product_id,  numberOfOrders, income) VALUES('f5c8aa3e-8932-46d7-851c-227a55d11459', NOW(), '8e448ed3-3999-4184-9fd8-300ce6d3beb3', 1, 17.33);
+INSERT INTO analytics(id, timstamp, product_id,  numberOfOrders, income) VALUES('463c9c18-7a56-42f2-af83-0f4290ece62b', NOW(), '56e16edf-0605-45bb-8ea3-b34b0d4c9fb7', 8, 700.45);
+INSERT INTO analytics(id, timstamp, product_id,  numberOfOrders, income) VALUES('6c067ac7-ee76-4499-b68d-d17c1335b7f6', NOW(), 'd29a07f7-24e2-4cc2-8aac-1e99c91d5db0', 1, 17.33);
